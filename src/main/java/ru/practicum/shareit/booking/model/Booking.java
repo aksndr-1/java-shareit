15ad01841b4103ecfa13779dcc1bb21bb0bdd824
Arzamastsev Alexandr <aksndr-1@yandex.ru>
2025-03-28 00:31:50 +0300

    Документирование

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -5,13 +5,43 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * Класс, представляющий бронирование предмета.
+ *
+ * @author aksdnr-1
+ * @version 1.0
+ */
 @Data
 @Builder
 public class Booking {
+    /**
+     * Идентификатор бронирования.
+     */
     private Long id;
+
+    /**
+     * Время начала бронирования.
+     */
     private LocalDateTime start;
+
+    /**
+     * Время окончания бронирования.
+     */
     private LocalDateTime end;
+
+    /**
+     * Идентификатор предмета, который бронируется.
+     */
     private Long item;
+
+    /**
+     * Идентификатор пользователя, который бронирует предмет.
+     */
     private Long booker;
+
+    /**
+     * Статус бронирования.
+     */
     private BookingStatusType status;
 }
+
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -5,11 +5,33 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * Класс, представляющий запрос на предмет.
+ *
+ * @author aksndr-1
+ * @version 1.0
+ */
 @Data
 @Builder
 public class ItemRequest {
+    /**
+     * Идентификатор запроса.
+     */
     private Long id;
+
+    /**
+     * Описание запроса.
+     */
     private String description;
+
+    /**
+     * Идентификатор автора запроса.
+     */
     private Long author;
+
+    /**
+     * Время создания запроса.
+     */
     private LocalDateTime createdTime;
 }
+
